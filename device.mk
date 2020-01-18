c3f527435fc1e1da8b49be8568a5e8e2d5afd172
Bruno Martins <bgcngm@gmail.com>
2020-01-18 19:04:13 +0000

    lmi: Import devicesettings
     * Base off LineageOS/android_device_xiaomi_sdm845-common.
    
    Change-Id: I0d461134e1a2513ad410d46527e7d7eb968d2ba9

--- device.mk
+++ device.mk
@@ -7,6 +7,10 @@
 # Inherit from sm8250-common
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
+# Device parts
+PRODUCT_PACKAGES += \
+    DeviceParts
+
 # Keylayout
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl
