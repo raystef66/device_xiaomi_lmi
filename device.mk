aa08c9818219c1fcdf2c0f5318a078b45270fab7
Anton Hansson <antonhansson@gmail.com>
2021-03-24 21:26:13 +0100

    lmi: Remove PRODUCT_AAPT config
    * This config strips unused drawable resources from the device
      to save space, but it is not particularly effective in practice
      (roughly ~30 MB across all partitions)
    
      Remove it to make the system image more generic
    
    Bug: 118676832
    Test: make
    Change-Id: I429dc449866fc0b147cade64c42cf41b00698ffe

--- device.mk
+++ device.mk
@@ -11,10 +11,6 @@ $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
