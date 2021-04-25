d5fd7ccb6587538e938e4f696c0a9d10241206e2
SebaUbuntu <barezzisebastiano@gmail.com>
2021-04-25 07:17:26 +0200

    lmi: Move common device.mk inclusion to the bottom
    Change-Id: I608e4442875fb46a2cf816863302c10db27276de

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,9 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from xiaomi sm8250-common
-include device/xiaomi/sm8250-common/BoardConfigCommon.mk
-
 DEVICE_PATH := device/xiaomi/lmi
 
 # Bluetooth
@@ -25,6 +22,9 @@ TARGET_KERNEL_CONFIG := vendor/lmi-perf_defconfig
 
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+
+# Inherit from xiaomi sm8250-common
+include device/xiaomi/sm8250-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
 include vendor/xiaomi/lmi/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -4,9 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from xiaomi sm8250-common
-$(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
@@ -28,6 +25,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
+
+# Inherit from xiaomi sm8250-common
+$(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/lmi/lmi-vendor.mk)
