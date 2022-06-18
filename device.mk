c703a7ba91d199dca7a38b5300040f89750d4c05
laleeroy <gabwielcastwo@gmail.com>
2022-06-18 20:27:50 +0800

    lmi: Build vendor.display.config@1.9
    getTransport: Cannot find entry vendor.display.config@1.9::IDisplayConfig/default in either framework or device VINTF manifest.
    
    [ERROR][FORMAT ] camxdisplayconfig.cpp:124 Initialize() IDisplayConfig service is NULL, version 1.9
    
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>
    Change-Id: I377ef0bd75719a1a1cb7385edf5a86a6f61fff83

--- device.mk
+++ device.mk
@@ -208,6 +208,8 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     memtrack.kona \
     vendor.display.config@1.0 \
+    vendor.display.config@1.9 \
+    vendor.display.config@1.9.vendor \
     vendor.display.config@1.11.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
