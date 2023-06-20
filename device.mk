3dd403d3bd658e483cdb3f88c622c973dd41f67b
John Macka <macka2004@gmail.com>
2023-06-20 19:18:11 +1000

    TEST: new miuicamera import

--- device.mk
+++ device.mk
@@ -327,7 +327,7 @@ PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
 # MiuiCamera
-$(call inherit-product-if-exists, vendor/xiaomi/lmi-miuicamera/products/miuicamera.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/lmi-miuicamera/config.mk)
 
 # Motor
 PRODUCT_PACKAGES += \
